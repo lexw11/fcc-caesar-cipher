43fa0e83ddc4bd5c37ff86c5fd128caa3c0be9f6
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-25 19:48:57 -0700

    Planned steps

--- rot13.js
+++ rot13.js
@@ -1,7 +1,11 @@
 console.log("connected");
 
 function rot13(str) {
-  
+  // Split string into array if single characters
+  // Check if character is letter
+  // Shift letter back 13 places
+  // Account for letters that need to wrap to end of alphabet
+  // Join new string
 }
 
 // Change the inputs below to test
